a76efebc5867536452af438db8e74ba7da42c77d
archish-edu <archisharun99@gmail.com>
2022-06-15 14:12:56 -0700

    dark mode!

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,51 @@
 import Link from 'next/link'
 import React from 'react'
 import Pdf from '../Documents/resume-current.pdf';
+import { useTheme } from 'next-themes';
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 
 
-function Header() {
-  return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
-        <div className="flex items-center space-x-5">
-            <Link href="/">
-                <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" />
-            </Link>
-            <div className="hidden md:inline-flex items-center space-x-5">
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
+const Header = () => {
+
+    const { systemTheme, theme, setTheme } = useTheme();
+
+    const renderThemeChanger = () => {
+
+        const currentTheme = theme === "system" ? systemTheme : theme;
+
+        if (currentTheme === "dark") {
+            return (
+                <SunIcon className="w-5 h-5 text-yellow-500 " role="button" onClick={() => setTheme('light')} />
+            )
+        }
+
+        else {
+            return (
+                <MoonIcon className="w-5 h-5 text-green-600 " role="button" onClick={() => setTheme('dark')} />
+            )
+        }
+    };
+
+    return (
+        <header className="flex justify-between p-5 max-w-7xl mx-auto">
+            <div className="flex items-center space-x-5">
+                <Link href="/">
+                    <h1 className="text-2xl font-bold">Archish Arun</h1> 
+                </Link>
+                <div className="hidden md:inline-flex items-center space-x-5">
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
+                </div>
             </div>
-        </div>
-        <div className="flex items-center space-x-5 text-green-600">
-            <h3> Sign In</h3>
-            <h3 className="border px-4 py-1 rounded-full border-green-600"> <a target="_blank" href="https://storage.cloud.google.com/archish-documents/Archish_Arun_Resume_15-06-2022-10-26-28.pdf">Resume</a> </h3>
-        </div>
-    </header>
-  )
+            <div className="flex items-center space-x-5 text-green-600">
+                <h3> Sign In</h3>
+                <>{renderThemeChanger()}</>
+                
+                <h3 className="border px-4 py-1 rounded-full border-green-600"> <a target="_blank" href="https://storage.cloud.google.com/archish-documents/Archish_Arun_Resume_15-06-2022-10-26-28.pdf">Resume</a> </h3>
+            </div>
+        </header>
+    )
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,9 @@ export default function Home({ posts }: Props) {
       </Head>
 
       <Header></Header>
-
+      <div>
+        <h1>Hello!</h1>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map(post => (
 
@@ -34,10 +36,11 @@ export default function Home({ posts }: Props) {
               <img 
                 className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" 
                 src={urlFor(post.mainImage).url()!} alt="" /> 
-              <div className="flex justify-between p-5 bg-white">
+              <div className="flex justify-between p-5 ">
               <div>  
                 <p className="text-lg font-bold">{post.title}</p>
-                <p className="text-xs">{post.description} by {post.author.name}</p>
+                <p className="text-xs">{post.description} </p>
+                <p className="text-xs py-2 text-green-600">by {post.author.name}</p>
               </div>
                 <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
               </div>
