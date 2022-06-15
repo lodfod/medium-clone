c92a363da6a51b3323c2b36facb004f3637cbd46
archish-edu <archisharun99@gmail.com>
2022-06-15 11:20:09 -0700

    resume to new tab

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ function Header() {
         </div>
         <div className="flex items-center space-x-5 text-green-600">
             <h3> Sign In</h3>
-            <h3 className="border px-4 py-1 rounded-full border-green-600"> <a href="https://storage.cloud.google.com/archish-documents/Archish_Arun_Resume_15-06-2022-10-26-28.pdf">Resume</a> </h3>
+            <h3 className="border px-4 py-1 rounded-full border-green-600"> <a target="_blank" href="https://storage.cloud.google.com/archish-documents/Archish_Arun_Resume_15-06-2022-10-26-28.pdf">Resume</a> </h3>
         </div>
     </header>
   )
